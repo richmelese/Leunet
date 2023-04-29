59dc9731005f039a0012f5c77bc3039639a10afa
Isaac Negatu <92875710+IsaacNegatuBA@users.noreply.github.com>
2023-04-29 18:36:47 +0300

    About page is now responsive

--- src/Components/DataCounter.jsx
+++ src/Components/DataCounter.jsx
@@ -4,20 +4,20 @@ export default function DataCounter({customClassName}) {
   return (
     <div className={`data_counter ${customClassName}` }>
         <div className='data_item'>
-            <p>100<span>M</span></p>
-            <p>Client Satisfaction</p>
+            <p className='num'>100<span>M</span></p>
+            <p className='desc'>Client Satisfaction</p>
         </div>
         <div className='data_item'>
-            <p>24<span>H</span></p>
-            <p>Expert Support Team</p>
+            <p className='num'>24<span>H</span></p>
+            <p className='desc'>Expert Support Team</p>
         </div>
         <div className='data_item'>
-            <p>98<span>k+</span></p>
-            <p>Sales Count</p>
+            <p className='num'>98<span>k+</span></p>
+            <p className='desc'>Sales Count</p>
         </div>
         <div className='data_item'>
-            <p>208<span>+</span></p>
-            <p>Client Worldwide</p>
+            <p className='num'>208<span>+</span></p>
+            <p className='desc'>Client Worldwide</p>
         </div>
     </div>
   )
